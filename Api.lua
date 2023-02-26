112ffebaf812e851454d994ca0f0b112d8c83ca9
Lolikarbuzik <73756261+Lolikarbuzik@users.noreply.github.com>
2023-02-26 13:41:57 +0100

    Add files via upload

--- Api.lua
+++ Api.lua
@@ -1,11 +1,17 @@
 --!strict
 
+-- if _G.TradingApi then
+--     return _G.TradingApi
+-- end
+
 local Signal: {new : () -> ()} = loadstring(game:GetObjects("rbxassetid://6654965987")[1].Source)()
 
 type TradeList = {Player}
 type Skins_Demands = { string : number }
 type Inventory = {}
 type Trade_Status = "Create" | "DeclineTrade" | "SendTrade" | "GotTrade"
+type AnalyzeResult = {Result : number,Reason : string}
+type Trade = {You : {}, Them : {}}
 
 local ProximityPromptService = game:GetService("ProximityPromptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -13,18 +19,42 @@ local Remotes = ReplicatedStorage.Remotes
 local SkinValues = ReplicatedStorage.Values
 
 local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local UI = Player.PlayerGui.UI
+
 
 local InventoryEvent:RemoteFunction = Remotes.Inventory
 local TradeEvent: RemoteEvent = Remotes.Trade
 
 local TradingApi:{
-	GotTrade : RBXScriptConnection,
+	GotTrade : RBXScriptSignal | {Destroy : () -> ()},
 	GetTradeRequests : () -> (TradeList),
-	SkinsList : Skins_Demands,
+	Skins : Skins_Demands,
 	Inventory : (Player?) -> Inventory,
-	Trade : (Trade_Status,Player ) -> ()
+	Trade : (Trade_Status : Trade_Status, PlayerName : string) -> (),
+	TradeAnalyzer : (Trade2 : TradeList, Trade1 : TradeList?) -> (AnalyzeResult),
+	GetTrade : () -> ()
 } = {}
-TradingApi.GotTrade = {}
+
+local function ExtractDataFromTemplate(Template)
+	return {
+		Player = Template:FindFirstChild("Them") and Players[Template:FindFirstChild("Them").Text] or Player,
+		Offer = (function()
+			local a = {}
+			for i,v in pairs(Template.Offers:GetChildren()) do
+				if v.ClassName~="UIGridLayout" then
+					a[v.NameLabel.Text] = tostring(v.Amount.Value:gsub("x",""))
+				end
+			end
+			return a
+		end)()
+	}
+end
+
+function TradingApi.GetTrade() : Trade
+	local TradeTemplate = UI:FindFirstChild("TradeTemplate")
+	return TradeTemplate and {You = ExtractDataFromTemplate(TradeTemplate.You), Them = ExtractDataFromTemplate(TradeTemplate.Them) } or nil
+end
 
 TradingApi.GotTrade = Signal.new()
 
@@ -34,22 +64,22 @@ TradeEvent.OnClientEvent:Connect(function(Trade_Status : Trade_Status, PlayerNam
     end
 end)
 
-function TradingApi:GetTradeRequests() : TradeList
+function TradingApi.GetTradeRequests() : TradeList
 	return {}
 end
 
 TradingApi.SkinsList = (function(List : Folder)
 	local Skins = {}
-	for _,SkinValue  in List:GetChildren() do
+	for _,SkinValue in pairs(List:GetChildren()) do
 		Skins[SkinValue.Name] = (SkinValue::IntValue).Value
 	end
 	return Skins
 end)(SkinValues)
 
-function TradingApi:Inventory(Player) : Inventory
+function TradingApi.Inventory(Player) : Inventory
 	return InventoryEvent:InvokeServer(Player)
 end
-function TradingApi:Trade(Player,Trade_Status)
+function TradingApi.Trade(Player,Trade_Status)
 	return TradeEvent:FireServer(Player,Trade_Status)
 end
 
--- main.lua
+++ main.lua
@@ -1,18 +1,52 @@
+local TradingPlaceId = 5325113759
+
+if game.PlaceId ~= TradingPlaceId then return warn("Game isnt a CB Trading") end
+
 type TradeList = {Player}
 type Skins_Demands = { string : number }
 type Inventory = {}
 type Trade_Status = "Create" | "DeclineTrade" | "SendTrade" | "GotTrade"
+type AnalyzeResult = {Result : number,Reason : string}
+local Tson = loadstring(game:HttpGet("https://pastebin.com/raw/9Tt6Ug7e"))()
 
 local TradingApi:{
 	GotTrade : RBXScriptSignal | {Destroy : () -> ()},
-	GetTraDeRequests : () -> (TradeList),
-	SkinsList : Skins_Demands,
+	GetTradeRequests : () -> (TradeList),
+	Skins : Skins_Demands,
 	Inventory : (Player?) -> Inventory,
-	Trade : (Trade_Status : Trade_Status, PlayerName : string) -> ()
+	Trade : (Trade_Status : Trade_Status, PlayerName : string) -> (),
+	TradeAnalyzer : (Trade2 : TradeList, Trade1 : TradeList?) -> (AnalyzeResult),
+	GetTrade : () -> ()
 } = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lolikarbuzik/CB_Trading_Bot/main/Api.lua"))()
 
-print("Trading Bot started",TradingApi)
+local lib = loadstring(game:HttpGet("https://pastebin.com/raw/dm6dfm15"))()
+local win = lib:AddWindow("BTrade 1.0",{
+	main_color = Color3.fromRGB(41, 74, 122),
+	min_size = Vector2.new(500, 600),
+	toggle_key = Enum.KeyCode.RightShift,
+	can_resize = true,
+})
 
-TradingApi.GotTrade:Connect(function(plr)
-    --print("Got Trade from",plr.Name)
+local main = win:AddTab("Trading")
+
+-- elements
+do
+	--[[
+	1 Trade analyzer:
+		• Check if player is currently trading if not. Tell player he isnt trading
+		• Get AnalyzeResult from TradeAnalyzer
+		• Tell Player AnalyzeResult
+	2. 
+	]]
+
+	local AnalyzerBtn = win:AddButton("Trade Analyzer",function()
+		if not TradingApi.GetTrade() then return end
+		local AnalyzeResult = TradingApi:TradeAnalyzer()
+	end)
+end
+
+TradingApi.GotTrade:Connect(function()
+	
 end)
+
+print(Tson(TradingApi.Skins))
